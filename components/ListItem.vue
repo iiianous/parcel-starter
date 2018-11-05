1d3281342f9b5c3848a3d969334242c9643538ce
Ian Mostar <ian@rallyhr.com>
2018-11-06 00:31:16 +0800

    added component default folder

---
+++ components/ListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li
+      v-for="(item, index) in lists"
+      :key="index"
+      >{{item.name}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lists: [
+        {
+          id: 1,
+          name: 'list one'
+        },
+        {
+          id: 2,
+          name: 'list two'
+        },
+        {
+          id: 3,
+          name: 'list three'
+        }
+      ]
+    }
+  }
+}
+</script>
--- views/FrontPage.vue
+++ views/FrontPage.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="h-full">
         <h1>{{msg}}</h1>
+        <ListItem />
     </div>
 </template>
 
 <script>
-
+import ListItem from '~/components/ListItem'
 export default {
     data() {
         return {
             msg: 'Hello World'
         }
     },
+
+    components: {
+        ListItem,
+    }
 }
 </script>
 
